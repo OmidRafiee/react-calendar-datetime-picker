fdcd8af2eca9e43bd7fe5ca5b22b654ae6b26d72
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2020-12-14 01:03:57 +0330

    Updates

---
+++ precache-manifest.4ee96a3cecb62dfeb2f294b6b175b6c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a68b7bbd56fd7e000542e963473f72b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28083ed789100d47d3d7",
+    "url": "./static/css/main.bb40ac15.chunk.css"
+  },
+  {
+    "revision": "5966bbac28980c770b81",
+    "url": "./static/js/2.7b03f600.chunk.js"
+  },
+  {
+    "revision": "00b54cc6383317353789a2f17549126c",
+    "url": "./static/js/2.7b03f600.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28083ed789100d47d3d7",
+    "url": "./static/js/main.1b927c16.chunk.js"
+  },
+  {
+    "revision": "ff1c3b0ab21d8dfcb19f",
+    "url": "./static/js/runtime-main.3571d6ef.js"
+  }
+]);
