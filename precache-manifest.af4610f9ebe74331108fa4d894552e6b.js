724703450e3f03bd239776c1ce7e780f6ef46e56
mmehdinasiri <m.mehdi.nasiri@gmail.com>
2021-02-06 00:11:41 +0330

    Updates

--- precache-manifest.af4610f9ebe74331108fa4d894552e6b.js
+++ precache-manifest.af4610f9ebe74331108fa4d894552e6b.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4e2d20e211c90252ebe1c73303f0d824",
+    "revision": "5cc87276803b896d38298593cb00c976",
     "url": "/react-calendar-datetime-picker/index.html"
   },
   {
-    "revision": "3bc4a8c8f46ae47a5558",
+    "revision": "ab01de690a8f5ce309ea",
     "url": "/react-calendar-datetime-picker/static/css/main.e02169b5.chunk.css"
   },
   {
-    "revision": "aaf564aea54237e56d7b",
-    "url": "/react-calendar-datetime-picker/static/js/2.f10340a7.chunk.js"
+    "revision": "26ffb91318a6885a3339",
+    "url": "/react-calendar-datetime-picker/static/js/2.397053df.chunk.js"
   },
   {
     "revision": "33ed5c48f6f261f0287d8a8f7ce4a41d",
-    "url": "/react-calendar-datetime-picker/static/js/2.f10340a7.chunk.js.LICENSE.txt"
+    "url": "/react-calendar-datetime-picker/static/js/2.397053df.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3bc4a8c8f46ae47a5558",
-    "url": "/react-calendar-datetime-picker/static/js/main.9c6cb4a4.chunk.js"
+    "revision": "ab01de690a8f5ce309ea",
+    "url": "/react-calendar-datetime-picker/static/js/main.70350ed3.chunk.js"
   },
   {
     "revision": "3ce3067ba4e0a2fcfa5b",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-calendar-datetime-picker/precache-manifest.92fe7e0a0be4e7abb8cfd98570657fc1.js"
+  "/react-calendar-datetime-picker/precache-manifest.af4610f9ebe74331108fa4d894552e6b.js"
 );
 
 self.addEventListener('message', (event) => {
